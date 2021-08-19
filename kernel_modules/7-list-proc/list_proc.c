c6629a88992bef44a15af6d604e49056de557190
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-08-19 10:26:41 +0000

    [Lab1] Add a code that print the process name and PID
    Changes to be committed:
    
        As required by the lab, the contents of the list_proc.c source were modified.
        Add the code for printing process name and PID that runs when the module loads and unloads,
        process name and PID of all processes that are running at the time the module loads.
        Code was created within a function that runs during module init, exit to allow code
        to run during module loading and unloading.
    
    	modified:   kernel_modules/7-list-proc/list_proc.c

--- kernel_modules/7-list-proc/list_proc.c
+++ kernel_modules/7-list-proc/list_proc.c
@@ -2,6 +2,8 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 /* TODO: add missing headers */
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 MODULE_DESCRIPTION("List current processes");
 MODULE_AUTHOR("Kernel Hacker");
@@ -9,11 +11,16 @@ MODULE_LICENSE("GPL");
 
 static int my_proc_init(void)
 {
-	struct task_struct *p;
+	struct task_struct *task = current;
 
 	/* TODO: print current process pid and its name */
+	pr_info("The pid of the current process %s is %u.\n", task->comm, task->pid);
 
 	/* TODO: print the pid and name of all processes */
+	printk("[Process list]\nPID\t\tNAME\n");
+	for_each_process(task){
+		printk("%u\t\t%s\n", task->pid, task->comm);
+	}
 
 	return 0;
 }
@@ -21,6 +28,8 @@ static int my_proc_init(void)
 static void my_proc_exit(void)
 {
 	/* TODO: print current process pid and name */
+	struct task_struct *task = current;
+	pr_info("The pid of the current process %s is %u.\n", task->comm, task->pid);
 }
 
 module_init(my_proc_init);
