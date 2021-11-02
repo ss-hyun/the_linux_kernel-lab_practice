2214c4bca0660e45a49e12f71c311d20825c230a
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-11-02 16:10:07 +0000

    [Lab5] Complete exercises

--- deferred_work/3-4-5-deferred/kernel/deferred.c
+++ deferred_work/3-4-5-deferred/kernel/deferred.c
@@ -38,10 +38,15 @@ struct mon_proc {
 static struct my_device_data {
 	struct cdev cdev;
 	/* TODO 1: add timer */
+	struct timer_list timer;
 	/* TODO 2: add flag */
+	unsigned int flags;
 	/* TODO 3: add work */
+	struct work_struct work;
 	/* TODO 4: add list for monitored processes */
+	struct list_head list;
 	/* TODO 4: add spinlock to protect list */
+	spinlock_t lock;
 } dev;
 
 static void alloc_io(void)
@@ -74,6 +79,10 @@ static struct mon_proc *get_proc(pid_t pid)
 
 
 /* TODO 3: define work handler */
+void work_handler(struct work_struct *work)
+{	
+	alloc_io();	
+}
 
 #define ALLOC_IO_DIRECT
 /* TODO 3: undef ALLOC_IO_DIRECT*/
@@ -81,13 +90,53 @@ static struct mon_proc *get_proc(pid_t pid)
 static void timer_handler(struct timer_list *tl)
 {
 	/* TODO 1: implement timer handler */
+	struct my_device_data *my_data = from_timer(my_data, tl, timer);
+
+	pr_info("[timer_handler] PID: %d, Taks name: %s\n", current->pid, current->comm);
+
 	/* TODO 2: check flags: TIMER_TYPE_SET or TIMER_TYPE_ALLOC */
+	switch (my_data->flags) {
+	case TIMER_TYPE_SET:
+		pr_info("Flag is TIMER_TYPE_SET\n");
+		break;
+	case TIMER_TYPE_ALLOC:
+		pr_info("Flag is TIMER_TYPE_ALLOC\n");
+		//alloc_io();
 		/* TODO 3: schedule work */
+		schedule_work(&my_data->work);
+		break;
+	case TIMER_TYPE_MON:
+	{
 		/* TODO 4: iterate the list and check the proccess state */
+		struct mon_proc *p, *t;
+
+		pr_info("Flag is TIMER_TYPE_MON\n");
+
+		spin_lock(&my_data->lock);
+		pr_info(KERN_ALERT "[timer_handler] Task list :");
+		list_for_each_entry_safe(p, t, &my_data->list, list) {
+			pr_info(KERN_CONT " %s(%d)", p->task->comm, p->task->pid);
 			/* TODO 4: if task is dead print info ... */
+			if (p->task->state == TASK_DEAD) {
+				pr_info("[timer_handler] Task %s(%d) is dead.\n", p->task->comm, p->task->pid);
 			/* TODO 4: ... decrement task usage counter ... */
+				put_task_struct(p->task);
 			/* TODO 4: ... remove it from the list ... */
+				list_del(&p->list);
 			/* TODO 4: ... free the struct mon_proc */
+				kfree(p);
+			}
+		}
+		pr_info(KERN_CONT "\n");
+		spin_unlock(&my_data->lock);
+
+		mod_timer(&my_data->timer, jiffies + 10*HZ);
+		break;
+	}
+	default:
+		break;
+	}
+		
 }
 
 static int deferred_open(struct inode *inode, struct file *file)
@@ -108,31 +157,57 @@ static int deferred_release(struct inode *inode, struct file *file)
 static long deferred_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	struct my_device_data *my_data = (struct my_device_data*) file->private_data;
+	volatile int i, j=2000*HZ;
 
 	pr_info("[deferred_ioctl] Command: %s\n", ioctl_command_to_string(cmd));
 
 	switch (cmd) {
 		case MY_IOCTL_TIMER_SET:
 			/* TODO 2: set flag */
+			my_data->flags = TIMER_TYPE_SET;
 			/* TODO 1: schedule timer */
+			mod_timer(&my_data->timer, jiffies + arg * HZ);
 			break;
 		case MY_IOCTL_TIMER_CANCEL:
 			/* TODO 1: cancel timer */
+			del_timer_sync(&my_data->timer);
 			break;
 		case MY_IOCTL_TIMER_ALLOC:
 			/* TODO 2: set flag and schedule timer */
+			my_data->flags = TIMER_TYPE_ALLOC;
+			mod_timer(&my_data->timer, jiffies + arg * HZ);
 			break;
 		case MY_IOCTL_TIMER_MON:
 		{
 			/* TODO 4: use get_proc() and add task to list */
-			/* TODO 4: protect access to list */
+			struct mon_proc *p = get_proc(arg);
+			if (IS_ERR(p))
+				return PTR_ERR(p);
 
+			/* TODO 4: protect access to list */
+			spin_lock_bh(&my_data->lock);
+			//spin_lock(&my_data->lock); -> dead lock 위험성
+			list_add(&p->list, &my_data->list);
+			while(j--){
+					i=20*HZ;
+					while(i--)
+						continue;
+			}
+			spin_unlock_bh(&my_data->lock);
+			//spin_unlock(&my_data->lock);
+			printk("while end\n");
+			
 			/* TODO 4: set flag and schedule timer */
+			my_data->flags = TIMER_TYPE_MON;
+			mod_timer(&my_data->timer, jiffies + 10*HZ);
+			printk("timer setting\n");
+
 			break;
 		}
 		default:
 			return -ENOTTY;
 	}
+	printk("return ioctl\n");
 	return 0;
 }
 
@@ -155,14 +230,20 @@ static int deferred_init(void)
 	}
 
 	/* TODO 2: Initialize flag. */
+	dev.flags = TIMER_TYPE_NONE;
+
 	/* TODO 3: Initialize work. */
+	INIT_WORK(&dev.work, work_handler);
 
 	/* TODO 4: Initialize lock and list. */
+	INIT_LIST_HEAD(&dev.list);
+	spin_lock_init(&dev.lock);
 
 	cdev_init(&dev.cdev, &my_fops);
 	cdev_add(&dev.cdev, MKDEV(MY_MAJOR, MY_MINOR), 1);
 
 	/* TODO 1: Initialize timer. */
+	timer_setup(&dev.timer, timer_handler, 0);
 
 	return 0;
 }
@@ -177,12 +258,20 @@ static void deferred_exit(void)
 	unregister_chrdev_region(MKDEV(MY_MAJOR, MY_MINOR), 1);
 
 	/* TODO 1: Cleanup: make sure the timer is not running after exiting. */
+	del_timer_sync(&dev.timer);
+
 	/* TODO 3: Cleanup: make sure the work handler is not scheduled. */
+	flush_scheduled_work();
 
 	/* TODO 4: Cleanup the monitered process list */
+	list_for_each_entry_safe(p, n, &dev.list, list) {
 		/* TODO 4: ... decrement task usage counter ... */
+		put_task_struct(p->task);
 		/* TODO 4: ... remove it from the list ... */
+		list_del(&p->list);
 		/* TODO 4: ... free the struct mon_proc */
+		kfree(p);
+	}
 }
 
 module_init(deferred_init);
--- deferred_work/6-kthread/kthread.c
+++ deferred_work/6-kthread/kthread.c
@@ -26,9 +26,14 @@ int my_thread_f(void *data)
 	pr_info("[my_thread_f] Current process id is %d (%s)\n",
 		current->pid, current->comm);
 	/* TODO: Wait for command to remove module on wq_stop_thread queue. */
+	wait_event_interruptible(wq_stop_thread, atomic_read(&flag_stop_thread) != 0);
 
 	/* TODO: set flag to mark kernel thread termination */
+	atomic_set(&flag_thread_terminated, 1);
+
 	/* TODO: notify the unload process that we have exited */
+	wake_up_interruptible(&wq_thread_terminated);
+
 	pr_info("[my_thread_f] Exiting\n");
 	do_exit(0);
 }
@@ -38,7 +43,13 @@ static int __init kthread_init(void)
 	pr_info("[kthread_init] Init module\n");
 
 	/* TODO: init the waitqueues and flags */
+	init_waitqueue_head(&wq_stop_thread);
+	init_waitqueue_head(&wq_thread_terminated);
+	atomic_set(&flag_stop_thread, 0);
+	atomic_set(&flag_thread_terminated, 0);
+	
 	/* TODO: create and start the kernel thread */
+	kthread_run(my_thread_f, NULL, "my_thread");
 
 	return 0;
 }
@@ -46,7 +57,12 @@ static int __init kthread_init(void)
 static void __exit kthread_exit(void)
 {
 	/* TODO: notify the kernel thread that its time to exit */
+	atomic_set(&flag_stop_thread, 1);
+	wake_up_interruptible(&wq_stop_thread);
+
 	/* TODO: wait for the kernel thread to exit */
+	wait_event_interruptible(wq_thread_terminated, atomic_read(&flag_thread_terminated) != 0);
+
 	pr_info("[kthread_exit] Exit module\n");
 }
 
